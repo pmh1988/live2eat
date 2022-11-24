1793d7f778a4bd98b0e9d4a85bd29d5908718066
Shyann Seet <shyann.seet@gmail.com>
2022-11-24 13:19:16 +0800

    made updates to frontend

--- live2eat_FrontEnd/app.py
+++ live2eat_FrontEnd/app.py
@@ -9,47 +9,65 @@ st.set_page_config(
     layout="centered",  # wide
     initial_sidebar_state="auto")  # collapsed
 '''
-## Live2Eat Food Tracking
-#Take the hard work out of tracking your food
+# Live2Eat Food Tracking
+Take the hard work out of tracking your food
 
 '''
 
 data = 'https://youtu.be/YaHtE96uc2c'
 st.video(data, format="video/mp4", start_time=0)
 
-with st.form(key='params_for_api'):
+col1, col2, col3, col4 = st.columns(4)
 
-    pickup_date = st.date_input('pickup datetime',
-                                value=datetime.datetime(
-                                    2012, 10, 6, 12, 10, 20))
-    pickup_time = st.time_input('pickup datetime',
-                                value=datetime.datetime(
-                                    2012, 10, 6, 12, 10, 20))
-    pickup_datetime = f'{pickup_date} {pickup_time}'
-    pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
-    pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
-    dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
-    dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
-    passenger_count = st.number_input('passenger_count',
-                                      min_value=1,
-                                      max_value=8,
-                                      step=1,
-                                      value=1)
+with col1:
+    st.header("Bak Chor Mee")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
 
-    st.form_submit_button('Make prediction')
+with col2:
+    st.header("Kaya Toast")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
 
-params = dict(pickup_datetime=pickup_datetime,
-              pickup_longitude=pickup_longitude,
-              pickup_latitude=pickup_latitude,
-              dropoff_longitude=dropoff_longitude,
-              dropoff_latitude=dropoff_latitude,
-              passenger_count=passenger_count)
+with col3:
+    st.header("Mee Siam")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
 
-wagon_cab_api_url = 'https://taxifare.lewagon.ai/predict'
-response = requests.get(wagon_cab_api_url, params=params)
+with col4:
+    st.header("Hokkien Mee")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
 
-prediction = response.json()
+# with st.form(key='params_for_api'):
 
-pred = prediction['fare']
+#     pickup_date = st.date_input('pickup datetime',
+#                                 value=datetime.datetime(
+#                                     2012, 10, 6, 12, 10, 20))
+#     pickup_time = st.time_input('pickup datetime',
+#                                 value=datetime.datetime(
+#                                     2012, 10, 6, 12, 10, 20))
+#     pickup_datetime = f'{pickup_date} {pickup_time}'
+#     pickup_longitude = st.number_input('pickup longitude', value=40.7614327)
+#     pickup_latitude = st.number_input('pickup latitude', value=-73.9798156)
+#     dropoff_longitude = st.number_input('dropoff longitude', value=40.6413111)
+#     dropoff_latitude = st.number_input('dropoff latitude', value=-73.7803331)
+#     passenger_count = st.number_input('passenger_count',
+#                                       min_value=1,
+#                                       max_value=8,
+#                                       step=1,
+#                                       value=1)
 
-st.header(f'Calories Recorded: ${round(pred, 2)}')
+#     st.form_submit_button('Make prediction')
+
+# params = dict(pickup_datetime=pickup_datetime,
+#               pickup_longitude=pickup_longitude,
+#               pickup_latitude=pickup_latitude,
+#               dropoff_longitude=dropoff_longitude,
+#               dropoff_latitude=dropoff_latitude,
+#               passenger_count=passenger_count)
+
+# wagon_cab_api_url = 'https://taxifare.lewagon.ai/predict'
+# response = requests.get(wagon_cab_api_url, params=params)
+
+# prediction = response.json()
+
+# pred = prediction['fare']
+
+st.header(f'Total Calories: ${round(pred, 2)}')
