74f011ee91f90448ce6753f19526bcc8b8ac1eec
Shyann Seet <shyann.seet@gmail.com>
2022-12-03 16:29:04 +1100

    enumerate dic

--- live2eat_FrontEnd/app.py
+++ live2eat_FrontEnd/app.py
@@ -182,7 +182,7 @@ st.title("Dishes detected")
 
 cols = st.columns(len(dishes_predicted_list))
 
-for dic in dishes_predicted_list:
+for i, dic in enumerate(dishes_predicted_list):
 
     image_opened = Image.open(dic['dish_images'])
     cols[i].image(image_opened)
