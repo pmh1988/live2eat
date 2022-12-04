a5072a75a4d28ed080769bd930c6779529cd15dc
Shyann Seet <shyann.seet@gmail.com>
2022-12-05 08:24:41 +1100

    Added model to iteration text

--- live2eat_FrontEnd/app.py
+++ live2eat_FrontEnd/app.py
@@ -193,7 +193,7 @@ bar = st.progress(0)
 
 for i in range(100):
     # Update the progress bar with each iteration.
-    latest_iteration.text(f'Iteration {i+1}')
+    latest_iteration.text(f'Model Iteration {i+1}')
     bar.progress(i + 1)
     time.sleep(0.1)
 '...and now we\'re done!'
