f0d3b227d0d14ebc83a0f3e92cd87b78efe314a0
Shyann Seet <shyann.seet@gmail.com>
2022-12-05 08:08:48 +1100

    changed model back to model2 as chicken rice hprediction worsened

--- live2eat_FrontEnd/food_prediction.py
+++ live2eat_FrontEnd/food_prediction.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 def predict():
 
-    model = load_model(os.path.join(os.getcwd(), 'my_model5.h5'))
+    model = load_model(os.path.join(os.getcwd(), 'my_model2.h5'))
     test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
     predict_data = test_datagen.flow_from_directory(os.path.join(
         os.getcwd(), 'data'),
